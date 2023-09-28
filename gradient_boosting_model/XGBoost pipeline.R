830f1aa09fdd794fffb0d476470776645696654a
Iván Andreu-Moreno <146280214+Ivan-Andreu@users.noreply.github.com>
2023-09-28 19:08:19 +0200

    Add files via upload

--- gradient_boosting_model/XGBoost pipeline.R
+++ gradient_boosting_model/XGBoost pipeline.R
@@ -6,8 +6,11 @@ library(Matrix)
 library(tidyverse)
 library(doParallel)
 library(parallel)
+library(pROC)
 ##Dataset----
-load("./Viral_Receptor_Prediction_Dataset.RData") #Modify the address to the location of the dataset file.
+load("./Viral_Receptor_Prediction_Dataset.RData") #Modify the address to the location of the dataset file. 
+#Data: contains all variables needed for the boosting model.
+#Gene_info: contains gen names and ensembl and uniprot codes. 
 label <- Data[,1] # Extraction of the objective variable for future steps.
 
 #BAYESIAN OPTIMIZATION ----
@@ -123,18 +126,19 @@ XGBModel=function(iter){
   l <- lapply(l,function(x)list(x))
   
   #Fit xgb model
-  fitxgb <- xgb.cv(data = my_xgb_df, nrounds = 10000, nthread = 0,missing = NA,params = l,scale_pos_weight = sum(label==0)/sum(label==1),
+  xgb_model_CV <- xgb.cv(data = xgbData, nrounds = 10000, nthread = 0,missing = NA,params = l,scale_pos_weight = sum(label==0)/sum(label==1),
                    metrics = "auc",early_stopping_rounds = 50,nfold = 10,verbose = F,
                    objective = "binary:logistic",prediction = T,showsd = T,stratified = T,maximize = T)
   #Return Output
-  return(fitxgb)
+  return(xgb_model_CV)
 }
 
 ##Running XGBModel replicates in parallel ----
+iter <- 1:(N*replicates) #Number of iterations in parallel
 no_cores <- detectCores() #Or select the desired amount of cores to use.  
 cl <- makeCluster(no_cores) #Cluster creation
 registerDoParallel(cl)
-clusterExport(cl,varlist = c("my_df_sparse","label","Parms_grid")) #Export data to clusters
+clusterExport(cl,varlist = c("Data","label","Parms_grid")) #Export data to clusters
 clusterEvalQ(cl,expr= { #Load xgboost library to use in our parallelized function
   library(xgboost)
 })
@@ -152,7 +156,7 @@ for(i in 1:length(z)){
   Logs[[i]] <- z[[i]]$evaluation_log
   Output <- rbind(Output,cbind(Parms_grid[i,],z[[i]]$evaluation_log[z[[i]]$best_iteration,]))
   print(i)}
-#Create a dataframe with metrics (VN, FP, FN, VP, Precision, Recall, F1, Mean_PR, AUC) for each hyperparamenter set
+#Create a dataframe with metrics (TN, FP, FN, TP, Precision, Recall, F1, Mean_PR, AUC) for each hyperparamenter set
 XGBMetrics <- data.frame()
 for (i in 1:length(Preds)){
   conf_mat <- as.vector(table(pred=as.integer(Preds[[i]]>0.5),lab=label))#Confusion matrix based on model predictions
@@ -162,7 +166,7 @@ for (i in 1:length(Preds)){
     f1 <- 2/(1/precision+1/recall) #F1 score: harmonic mean of precision and recall
     mean_precision_recall <- (precision+recall)/2 #Combined precision and recall (average)
     XGBMetrics[i,1:8] <- c(conf_mat,precision,recall,f1,mean_precision_recall)}}
-colnames(XGBMetrics) <- c("VN","FP","FN","VP","Precision","Recall","F1","Mean_PR")#Set data frame column names.
+colnames(XGBMetrics) <- c("TN","FP","FN","TP","Precision","Recall","F1","Mean_PR")#Set data frame column names.
 
 ModelID <- rep(1:N,each=replicates) #ModelID to differentiate replicates of each hyperparameter set.
 #Add ModelID and cross validation test auc mean to the XGBMetrics dataframe.
@@ -172,7 +176,7 @@ XGBMetrics <- cbind(ModelID,Output[,c(1:8)],XGBMetrics,AUC=Output$test_auc_mean)
 ##Creating a data frame with mean and variance of metrics for each ModelID ----
 XGBM1 <- XGBMetrics %>% group_by(ModelID)%>%summarise(across(1:ncol(Parms_grid),mean))
 XGBM2 <- XGBMetrics%>%group_by(ModelID)%>%summarise(across((ncol(Parms_grid)+1):(ncol(XGBMetrics)-1),list(avg=mean,var=var)))
-XGBMeanMetrics <- cbind(XGBM1,XGBM2)
+XGBMeanMetrics <- cbind(XGBM1,XGBM2[,-1])
 #View(XGBMeanMetrics) #Run to visualize the output
 
 ##Deciding the best model ----
@@ -201,6 +205,25 @@ plot_meanPR
 Best_model_ID <- Best_model%>%filter(p_value<0.01)%>%filter(mu_mPR==max(mu_mPR))%>%pull(model) #If more than one model appears reduce delta in previous steps (line 184). 
 Best_model_ID
 
+##Average best model prediction and metrics ----
+preds <- unlist(Preds) #Unlist N*replicates cross validation model predictions.
+dim(preds) <- c(5112,N*replicates) #Give them proper dimensions.
+preds <- as.data.frame(cbind(ModelID,t(preds))) #Generate a data frame with model ID and predictions per row.
+mean_preds <- preds%>%group_by(ModelID)%>%summarise(across(everything(),mean)) #Calculate averaged predictions for each model.
+Avg_Pred <- cbind(Gene_info,Avg_model_pred=t(mean_preds[Best_model_ID,-1]),label) #Save averaged predictions for the best model.
+
+#Calculate metrics based on averaged best model predictions.
+conf_mat <- as.vector(table(Avg_Pred$Avg_model_pred>0.5,Avg_Pred$label))#Confusion matrix based on averaged best model predictions
+precision <- conf_mat[4]/sum(conf_mat[c(2,4)])#Precision = TP/(TP+FP) What fraction of the predicted receptors are real (or known).
+recall <- conf_mat[4]/sum(conf_mat[c(3,4)])#Recall = TP/(FN+TP) What fraction of the known receptors is recovered by the average best model prediction.
+f1 <- 2/(1/precision+1/recall) #F1 score: harmonic mean of precision and recall
+Mean_PR <-  (precision+recall)/2 #Averaged precision and recall
+AUC <-  pROC::auc(label,Avg_Pred$Avg_model_pred) #AUC 
+
+#Best Model metrics based on averaged cross validation predictions
+Avg_best_model_metrics <- data.frame(XGBM1[Best_model_ID,], TN = conf_mat[1], FP = conf_mat[2], FN = conf_mat[3], TP = conf_mat[4],
+                                     Precision = precision, Recall = recall, F1 = f1, Mean_PR = Mean_PR, AUC = AUC);Avg_best_model_metrics
+
 #TRAINING XGBOOST MODEL AND GETTING VARIABLE (FEATURE) IMPORTANCE ----
 
 ##Train XGBoost Model with best parameter combination and average nrounds (iterations) ----
@@ -210,7 +233,7 @@ l <- lapply(l,function(x)list(x)) #List of hyperparameters
 
 set.seed(42)
 xgbData <- xgboost::xgb.DMatrix(data = Data[,-1], label=label, missing = NA)
-XGBOOST_Model <- xgboost(data = my_xgb_df,missing = NA,params = l,nrounds = DF%>%filter(ModelID==Best_model_ID)%>%pull(iter_avg)%>%round(),verbose = T,scale_pos_weight = sum(label==0)/sum(label==1))
+XGBOOST_Model <- xgboost(data = xgbData,missing = NA,params = l,nrounds = XGBMeanMetrics%>%filter(ModelID==Best_model_ID)%>%pull(iter_avg)%>%round(),verbose = T,scale_pos_weight = sum(label==0)/sum(label==1))
 ##Getting variable importance ----
 importance <- xgb.importance(model = XGBOOST_Model)
 #View(importance)#Run to see dataframe
